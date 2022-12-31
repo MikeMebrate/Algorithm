f3c70f47270445af92a28e07c2ab6cd465097a00
mike <Mikiyasmebrate@gmail.com>
2022-12-31 10:52:44 +0300

    BinarySearch.java

--- SearchingAlgorithm/BinarySearch.java
+++ SearchingAlgorithm/BinarySearch.java
@@ -14,10 +14,22 @@ public class BinarySearch {
          }
      return -1;
     }
+
+    public static int binaryRecursive(int[] list, int item, int high, int low){
+       if(low <= high){
+           int mid= (high+low)/2;
+           if(list[mid] == item) return mid;
+           else if(list[mid] > item) return binaryRecursive(list,item,mid-1,low);
+           else return binaryRecursive(list,item,high,mid+1);
+       }
+       return -1;
+    }
     public static void main(String[] args) {
             System.out.println(binarySearch(new int[] {1,4,5,6,7,9,11,14,15},14));
             System.out.println(binarySearch(new int[] {1,4,5,6,7,9,11,14,15},4));
             System.out.println(binarySearch(new int[] {1,4,5,6,7,9,11,14,15},17));
 
+      System.out.println("Recursive");
+        System.out.println(binaryRecursive(new int[] {1,4,5,6,7,9,11,14,15},7, 8,0));
     }
 }
